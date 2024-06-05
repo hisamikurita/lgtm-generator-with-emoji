d368acf63a2cc795f1dc98d7fdfd0f0ef7a12311
hisamikurita <kuritahisami@gmail.com>
2024-06-06 01:34:22 +0900

    feat: corsも変更！

---
+++ backend/config/cors.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'paths' => ['*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
