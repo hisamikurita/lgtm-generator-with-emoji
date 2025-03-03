9878f7435d24fb8a8b284a204e56559e7cfa867d
hisamikurita <kuritahisami@gmail.com>
2025-03-03 20:59:26 +0900

    feat:

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
+
+if (App::environment('production') || App::environment('staging')) {
+    URL::forceScheme('https');
+}
 
 Route::get('/', function () {
     return view('welcome');
